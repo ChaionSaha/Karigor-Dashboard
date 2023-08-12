d4222c02e4abc68409b908b0a01601ae255eaae1
Chaion Saha <chaionsaha00@gmail.com>
2023-08-12 22:41:07 +0600

    initiated first commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./**/*.{html,js}'],
+	theme: {
+		extend: {},
+		screens: {
+			sm: '576px',
+			md: '768px',
+			lg: '992px',
+			xl: '1200px',
+			'2xl': '1400px',
+		},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: [
+			{
+				mytheme: {
+					primary: '#2F6ADE',
+					secondary: '#00965C',
+					accent: '#280559',
+					'base-100': '#ffffff',
+					'base-200': '#7c7c7c ',
+					success: 'rgba(40, 199, 111, 0.16)',
+					'success-content': '#28C76F',
+					warning: 'rgba(255, 198, 0, 0.16)',
+					'warning-content': '#FFC600',
+					error: 'rgba(189, 0, 255, 0.16)',
+					'error-content': '#BD00FF',
+				},
+			},
+		],
+	},
+};
